2e52709c2f297b8f093727cf9d64aa24e41434cb
Nagaraju99 <m.v.nagaraju0007@gmail.com>
2026-03-26 22:34:40 +0530

    Initial commit with latest implementation

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+// import { Link } from "react-router-dom";
+// import Button from "../Button/Button";
+import Logo from "../Logo/Logo";
+import Search from "../Search/Search";
+import styles from "./Navbar.module.css";
+import { Button } from "@mui/material";
+import { Link } from "react-router-dom";
+
+function Navbar({ searchData }) {
+  return (
+    <nav className={styles.navbar}>
+      <Link to="/">
+        <Logo />
+      </Link>
+      <Search
+        placeholder="Search a song of your choice"
+        searchData={searchData}
+      />
+      <Button className={styles.btnfeed}>Give Feedback</Button>
+    </nav>
+  );
+}
+
+export default Navbar;
---
+++ src/helpers/helpers.js
@@ -0,0 +1,5 @@
+// src/helpers/helpers.js
+export function truncate(str, maxLength) {
+  if (!str) return "";
+  return str.length > maxLength ? str.slice(0, maxLength) + "…" : str;
+}
