6de0505e365c79f31c28341bc1440ef597cd8c7b
Nagaraju99 <m.v.nagaraju0007@gmail.com>
2026-03-27 16:32:51 +0530

    QTify updated code

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import CardItem from '../CardItem/CardItem'
-import { Grid, Typography } from '@mui/material'
+import { Button, Grid, Typography } from '@mui/material'
 
 const Cards= ({albumData}) => {
   return (<>
-    <Grid container justifyContent="space-between"><Typography sx={{textAlign:"left", paddingLeft:"30px", fontFamily:"Poppins", color:"#ffffff", marginBottom:"20px"}}>Top Albums</Typography> <Typography sx={{textAlign:"right", paddingRight:"30px", fontFamily:"Poppins", color:"#34C94B", marginBottom:"20px"}}>Collapse</Typography></Grid>
+    <Grid container justifyContent="space-between"><Typography sx={{textAlign:"left", paddingLeft:"30px", fontFamily:"Poppins", color:"#ffffff", marginBottom:"20px"}}>Top Albums</Typography> <Button sx={{textAlign:"right", paddingRight:"30px", fontFamily:"Poppins", color:"#34C94B", marginBottom:"20px"}}>Collapse</Button></Grid>
     
     <Grid container spacing={7} justifyContent="flex-start" padding="0px 30px">
       {albumData.map((eachItem, index) => (
