14260b15fd64f7c3c2b6e325c86567cb813a002b
Nagaraju99 <m.v.nagaraju0007@gmail.com>
2026-03-27 18:45:51 +0530

    Small update to QTify

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -4,66 +4,49 @@ import Carousel from "../Carousel/Carousel";
 import { Button, Grid, Typography } from "@mui/material";
 
 const Cards = ({ albumData, title }) => {
-  const [collapsed, setCollapsed] = useState(false);
+  const [showCarousel, setShowCarousel] = useState(false);
 
-  const handleToggle = () => setCollapsed(!collapsed);
+  const handleToggle = () => setShowCarousel(!showCarousel);
 
   return (
     <>
       <Grid container justifyContent="space-between">
-        <Typography
-          sx={{
+        <Typography sx={{
             textAlign: "left",
             paddingLeft: "30px",
             fontFamily: "Poppins",
             color: "#ffffff",
             marginTop: "30px",
-          }}
-        >
+          }}>
           {title}
         </Typography>
-        <Button
-          onClick={handleToggle}
-          sx={{
+        <Button onClick={handleToggle} sx={{
             textAlign: "right",
             paddingRight: "30px",
             fontFamily: "Poppins",
             color: "#34C94B",
             marginTop: "30px",
-          }}
-        >
-          {collapsed ? "Show All" : "Collapse"}
+          }}>
+          {showCarousel ? "Show All" : "Collapse"}
         </Button>
       </Grid>
 
-      {collapsed ? (
+      {showCarousel ? (
         <Carousel
           data={albumData}
           renderItem={(album) => <CardItem eachItem={album} />}
         />
       ) : (
-        <Grid container spacing={2} justifyContent="flex-start" padding="0px 30px" marginBottom="50px">
+        <Grid container spacing={3} sx={{ pl: 3, mb: 6 }}>
           {albumData.map((album) => (
             <Grid
               item
               key={album.id}
-          sx={{
-              flexBasis: {
-                xs: "calc(100% / 2)",   // 2 items per row
-                sm: "calc(100% / 3)",   // 3 items per row
-                md: "calc(100% / 5)",   // 5 items per row
-                lg: "calc(100% / 7)",   // 7 items per row
-              },
-              maxWidth: {
-                xs: "calc(100% / 2)",
-                sm: "calc(100% / 3)",
-                md: "calc(100% / 5)",
-                lg: "calc(100% / 7)",
-              },
-
-              mb: "70px", // margin-bottom shorthand
-            }}
-
+              sx={{
+                flexBasis: { xs: "50%", sm: "33.33%", md: "20%", lg: "14.28%" },
+                maxWidth: { xs: "50%", sm: "33.33%", md: "20%", lg: "14.28%" },
+                mb: "70px",
+              }}
             >
               <CardItem eachItem={album} />
             </Grid>
@@ -73,5 +56,4 @@ const Cards = ({ albumData, title }) => {
     </>
   );
 };
-
 export default Cards;
